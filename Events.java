2b83c283054382f71e86abb7d70ffca378a9df00
Legenduz <45770956+Legenduz@users.noreply.github.com>
2020-02-15 23:20:51 +0500

    Create Events.java

---
+++ Events.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class EventsEx extends WindowAdapter implements MouseMotionListener{
+    JFrame frame;
+    JLabel coordinates;
+    // setting layout
+    public void initGUI ( ) {
+        // creating event generator
+        frame = new JFrame();
+        Container cont = frame.getContentPane();
+        cont.setLayout(new BorderLayout( ) );
+        coordinates = new JLabel ();
+        cont.add(coordinates, BorderLayout.NORTH);
+        // registring mouse event handler with generator
+        frame.addMouseMotionListener(this);
+        frame.addWindowListener(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        frame.setVisible(true);
+    } // end initGUI method
+    //default constructor
+    public EventsEx ( ) {
+        initGUI();
+    }
+    // MouseMotionListener event hadler handling dragging
+    public void mouseDragged (MouseEvent me) {
+        int x = me.getX();
+        int y = me.getY();
+        coordinates.setText("Dragged at [" + x + "," + y + "]");
+    }
+    // MouseMotionListener event handler handling motion
+    public void mouseMoved (MouseEvent me) {
+        int x = me.getX();
+        int y = me.getY();
+        coordinates.setText("Moved at [" + x + "," + y + "]");
+    }
+    public void windowActivated (WindowEvent we) { }
+    public void windowClosed (WindowEvent we) {
+
+    }
+    public void windowClosing (WindowEvent we) {
+        JOptionPane.showMessageDialog(null, "Good Bye");
+        System.exit(0);
+    }
+    public void windowDeactivated (WindowEvent we) { }
+    public void windowDeiconified (WindowEvent we) { }
+    public void windowIconified (WindowEvent we) { }
+    public void windowOpened (WindowEvent we) { }
+
+} // end class
